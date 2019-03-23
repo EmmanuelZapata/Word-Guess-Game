038726179c72662fd35b4c399763561311c73ba5
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-23 03:48:57 -0500

    started on css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,7 @@
-
+//create array of words
+//choose random word from array
+//create underscores based on word choosen
+//get user guess
+//check if guess is right
+//make change to underscore if right
+//make change to underscore if wrong
