afac6c2f1153afd2d910c65e943b23f37629f5c2
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-25 20:12:15 -0500

    created random selection fo words and then split them

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
 //create array of words
-var word = ["taco", "burger", "pizza", "salad", "steak"];
 //choose random word from array
+function words(){
+    var word = ["taco", "burger", "pizza", "salad", "steak"];
+    var random = [Math.floor(Math.random() * 5)];
+    return word[random].split('')
+};
+
+
 //create underscores based on word choosen
 //get user guess
 //check if guess is right
