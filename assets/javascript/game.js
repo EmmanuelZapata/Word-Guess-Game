f71bef343306e0d6e0201fa0e7e7adb1e095c4c1
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-27 17:07:47 -0500

    added document.getelementbyId for all id's in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,17 @@
+//Connecting html to javascript that we want to be able to change.
+
+var displayWord = document.getElementById("word") 
+var winText = document.getElementById("win-text");
+var guessCount = document.getElementById("guesses-count");
+var guessLetters = document.getElementById("guesses-letters");
+var guessesLeft = document.getElementById("guesses-left")
+
+// alphabet array and word arrays
+
+var letters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y", "z"];
+
+
+
 //create array of words
 //choose random word from array
 // function words(){
@@ -5,15 +19,6 @@
 //     var random = [Math.floor(Math.random() * 5)];
 //     return word[random].split('')
 // };
-
-
-
-
-var DisplayWord = document.getElementById("word") 
-var WinText = document.getElementById("win-text");
-var GuessCount = document.getElementById("guesses-count");
-var GuessLetters = document.getElementById("guesses-letters");
-var letters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y", "z"];
 
 
 function callletters (pressedLetter){
