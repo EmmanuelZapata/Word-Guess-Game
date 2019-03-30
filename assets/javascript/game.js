f186efffd305fa264903c405764b051b16ed9b07
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-30 02:45:52 -0500

    trying to figure out user input var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,58 +10,13 @@ var guessesLetters = document.getElementById("guesses-letters")
 var computerLetters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y", "z"];
 var userGuess = [];
 //functions
+//Random letter choosen.  
 document.onkeyup = function(event) {
-    var random = [Math.floor(Math.random() * 5)];
-    return word[random].split('')
+    var userInput = String.toLowerCase(event.key);
+    console.log()
+    var computerChoice = computerLetters[Math.floor(Math.random()*computerLetters.length)];
+   
 };
-// displayWord.textContent = "Word: " + word();
-// const words = ["taco", "burger", "pizza", "salad", "steak"];
-// function chooseWord(array) {
-//     return array[Math.floor(Math.random() * array.length)].toUpperCase()
-//   }
-  
-//   function blankWord(word) {
-//     return word.replace(/w/g, '_')
-//   }
-  
-//   function fillInWordWithLetter(letter, word) {
-//     return word.toLowerCase().split('').map(l =>  {
-//       if (l === letter) {
-//         return l.toUpperCase()
-//       }
-//       return '_'
-//     }).join(' ')
-
-// function callletters (pressedLetter){
-// var isFound = false;
-//     for (var i = 0; i < letters.length; i ++){
-//         if (letters[i].toLowerCase() === pressedLetter){ 
-//             GuessCount[i] === pressedLetter;
-//             isFound = true;
-
-    
-//         }
-//     }
-// return isFound;
-// };
 
 
 
-
-
-// document.onkeyup = function(event){
-//     var userGuess = event.key;
-//     var pressedLetter = event.key;
-
-
-// guessLetters.textContent = "Guessed Letters: " + userGuess;
-// guessCount.textContent = "Guesses: " + pressedLetter;
-// }}
-
-
-
-//create underscores based on word choosen
-//get user guess
-//check if guess is right
-//make change to underscore if right
-//make change to underscore if wrong
