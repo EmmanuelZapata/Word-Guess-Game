eab4ea3be358b78779d10a1fe9b9221994dc8f4a
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-30 03:22:18 -0500

    took out variables to connect to html... doing it differently I think.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,25 @@
-//Connecting html to javascript that we want to be able to change.
-
-var displayWord = document.getElementById("wins") 
-var winText = document.getElementById("losses");
-var guessCount = document.getElementById("guesses-count");
-var guessesLetters = document.getElementById("guesses-letters")
-
 // alphabet array
-
 var computerLetters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y", "z"];
-var userGuess = [];
+var letters = [];
 //functions
 //Random letter choosen.  
 document.onkeyup = function(event) {
-    var userInput = String.toLowerCase(event.key);
+    //userInput is made into a lowercase  
+    var userInput = String(event.key).toLowerCase();
     console.log()
+    //computer makes a random choice from array
     var computerChoice = computerLetters[Math.floor(Math.random()*computerLetters.length)];
+    //userInput is pushed to guessLetters
+    letters = push(userInput);
+    //if and else if statement on choices
+    if (userInput == computerChoice) {
+     //adds a win to win count
+      displayWins++;
+     alert('You gussed right! You Won!');
+     //reseting the guesses back to 9 so that the user can play again
+     guessesCount = 9; 
+     letters.length = 0;
+    }
    
 };
 
