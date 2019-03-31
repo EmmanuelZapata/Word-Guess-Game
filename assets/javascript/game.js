9487245a8689023cc5507fe8941c2edacddc0d84
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-30 20:32:50 -0500

    count guesses... think I have it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,32 @@
 // alphabet array
 var computerLetters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y", "z"];
+
+// create the vars to 0
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var eachofLetters = null;
 var letters = [];
+
 //functions
+
 //Random letter choosen.  
 document.onkeyup = function(event) {
     //userInput is made into a lowercase  
     var userInput = event.key.toLowerCase();
-    console.log()
+    console.log(userInput)
+
     //computer makes a random choice from array
     var computerChoice = computerLetters[Math.floor(Math.random()*computerLetters.length)];
-    //userInput is pushed to guessLetters
-    letters = push(userInput);
+    
+   //count guesses left
+   
+    function countGuessesLeft() {
+        document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    }
+
+
+    
     //if and else if statement on choices
     if (userInput == computerChoice) {
      //adds a win to win count
